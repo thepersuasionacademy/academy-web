cc0def25abc390e374453d8403a709f706e59c31
thepowerark <jackson@thepowerark.com>
2025-01-30 12:56:40 -0500

    authentication bugs more

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -62,16 +62,3 @@ export default async function AuthCallbackPage({
     </div>
   )
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    '/((?!_next/static|_next/image|favicon.ico|public).*)',
-  ],
-}
